12674af8fc1f2c05c71c8869b8ffed853ee2b95f
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2022-09-14 23:10:33 +0600

    Reverse

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      // countdownOverlay.style.display = "flex";
-      countdownOverlay.style.display = "";
+      countdownOverlay.style.display = "flex";
+      // countdownOverlay.style.display = "";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
